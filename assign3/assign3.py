4bbbac9efa38b08424d6525d3da4cc5697d94662
Cam <camperdue42@gmail.com>
2020-10-15 18:22:20 -0400

    sorted has sum and partition

--- assign3/assign3.py
+++ assign3/assign3.py
@@ -18,8 +18,39 @@ def search(array, value):
     return final
 
 
+def sorted_has_sum(S, x):
+    array = S
+    result = False
+    for n in S:
+        looking_for = x - n
+        if search(array, looking_for):
+            result = True
+            break
+        else:
+            array = array[1:]
+    return result
+
+
+def partition(array, l, h):
+    x = array[h]
+    i = l - 1
+    for j in range(l, h):
+        if array[j] <= x:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+        j += 1
+    array[i+1], array[h] = array[h], array[i+1]
+    return i+1
+
+
+def quicksort(a):
+    return 0
+
+
+
 def main():
     print(search([1, 2, 3, 6, 10], 11))
+    print(sorted_has_sum([1, 2, 5], 6))
 
 
 if __name__ == "__main__":
